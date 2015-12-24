9e9ca31d5c35d069970a3fdaddfa648a4b6a8e69
Kwarthys <kwarthys@gmail.com>
2015-12-24 15:05:31 +0100

    Tutorial, should work.

--- app/src/main/java/hellyeah/spaceadventura/MainActivity.java
+++ app/src/main/java/hellyeah/spaceadventura/MainActivity.java
@@ -1,17 +1,49 @@
 package hellyeah.spaceadventura;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    SpaceView view;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Get a Display object to access screen details
+        Display display = getWindowManager().getDefaultDisplay();
+        // Load the resolution into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Initialize gameView and set it as the view
+        view = new SpaceView(this, size.x, size.y);
+        setContentView(view);
+    }
+
+    // This method executes when the player starts the game
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Tell the gameView resume method to execute
+        view.resume();
+    }
+
+    // This method executes when the player quits the game
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Tell the gameView pause method to execute
+        view.pause();
     }
 
 
